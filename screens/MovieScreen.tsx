24860b29cc66f065d7f31797cd8b1e981d712668
msulemanb <msulemanbutt39@gmail.com>
2026-04-08 14:12:02 +0500

    used react-native linear gradient and added movie details on MovieScreen

--- screens/MovieScreen.tsx
+++ screens/MovieScreen.tsx
@@ -9,11 +9,12 @@ import {
 import React, { useEffect, useState } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import LinearGradient from 'react-native-linear-gradient';
 import { ChevronLeftIcon } from 'react-native-heroicons/outline';
 import { HeartIcon } from 'react-native-heroicons/solid';
 import { styles, theme } from '../theme';
 import { height, width } from '../constants/dimensions';
-import { sampleImageUrls } from '../constants';
+import { sampleDescription1, sampleImageUrls } from '../constants';
 
 const MovieScreen = () => {
   const { params: item } = useRoute();
@@ -21,6 +22,7 @@ const MovieScreen = () => {
   const navigation = useNavigation();
   const ios = Platform.OS === 'ios';
   const topMargin = ios ? '' : 'mt-3';
+  let movieName = 'Ant man and the wasp quqntumania';
   useEffect(() => {
     // call the movies detail api
   }, [item]);
@@ -63,7 +65,37 @@ const MovieScreen = () => {
             }}
             style={{ height: height * 0.55, width }}
           />
+          <LinearGradient
+            colors={['transparent', 'rgba(23,23,23,0.5)', 'rgba(23,23,23,1)']}
+            style={{ width, height: height * 0.55 }}
+            start={{ x: 0.5, y: 0 }}
+            end={{ x: 0.5, y: 1 }}
+            className="absolute bottom-0"
+          />
         </View>
+      </View>
+      {/* movie details */}
+      <View style={{ marginTop: -height * 0.09 }} className="space-y-3">
+        {/* title */}
+        <Text className="text-white text-center text-3xl font-bold tracking-wider">
+          {movieName}
+        </Text>
+        {/* status, release, runtime*/}
+        <Text className="text-neutral-400 font-semibold text-base text-center">
+          Release | 2020 | 170 mins
+        </Text>
+        {/* genres */}
+        <View className="flex-row justify-center mx-4 space-x-2">
+          {['Action', 'Thriller', 'Comedy'].map((val, ind) => (
+            <Text className="text-neutral-400 font-semibold text-base text-center">
+              {val + (ind === 2 ? '' : ' | ')}
+            </Text>
+          ))}
+        </View>
+        {/* description */}
+        <Text className="text-neutral-400 mx-4 tracking-wide">
+          {sampleDescription1}
+        </Text>
       </View>
     </ScrollView>
   );
